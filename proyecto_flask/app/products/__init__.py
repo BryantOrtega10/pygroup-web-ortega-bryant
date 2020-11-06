1cf7b20119104cf36b62a310701c42c9b9585251
BryantOrtega10 <bryant.ortega1010@gmail.com>
2020-11-05 21:21:43 -0500

    Tarea 3

--- proyecto_flask/app/products/__init__.py
+++ proyecto_flask/app/products/__init__.py
@@ -4,6 +4,13 @@ tarea3_blueprint = Blueprint('tarea3', __name__, url_prefix='/tarea3')
 
 @tarea3_blueprint.route('/<string:name>')
 def index_tarea3(name):
+    """
+        Description: This method show a message for different values, if value is pygroup shows an error with code 400
+        parameters: name = String with a name 
+        return: Response    400 in case of value of parameter is "pygroup"
+                            200 in another case
+    """
+
     if name != "pygroup":
         return Response("Felicitaciones! Trabajo exitoso {}".format(name), status=200)
     return Response("ERROR! No se puede usar el nombre pygroup", status=400)
